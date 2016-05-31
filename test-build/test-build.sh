fe4e3c821f9870277a5faa86a5237fecb016e257
kvzfly <247463413@qq.com>
2016-05-31 14:58:18 -0400

    cicd

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -9,4 +9,4 @@ sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key 833795D1A9279F8A934E --secret-key QDs4JP1dHVdxRziCBC35RGxy1rrHSgh2TA1VkQwy -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --access-key 67D80D1F783AFD302842 --secret-key HY27yD11kxAFMQRsxgLVHeWW7zax9SrNc2Yu4W15 -p python-redis-demo-build${BUILD_NUMBER} up -d
