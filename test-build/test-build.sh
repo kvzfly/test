66d3395a79981db889c4deb28835cc176057303b
kvzfly <247463413@qq.com>
2016-05-31 11:51:25 -0400

    This is a New test

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd ${WORKSPACE}/src
-docker build -t 192.168.0.190:80/python-redis-demo:${BUILD_NUMBER} .
+docker build -t 192.168.5.36:5002/poc/python-redis-demo:${BUILD_NUMBER} .
 
-docker push 192.168.0.190:80/python-redis-demo:${BUILD_NUMBER}
+docker push 192.168.5.36:5002/poc/python-redis-demo:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key E0F535B1D46D84442A4F --secret-key bWySMz9vy9zcMxuhS3XBPg8hjbxVnt3k7nDsXaZo -p python-redis-demo-build4 up --pull -d --confirm-upgrade --upgrade pyapp
+./rancher-compose --access-key 833795D1A9279F8A934E --secret-key QDs4JP1dHVdxRziCBC35RGxy1rrHSgh2TA1VkQwy -p python-redis-demo-build${BUILD_NUMBER} up -d
